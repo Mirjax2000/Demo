e1eaa6778190a4139b0ca8e29694612489a8eb5a
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-11 19:55:09 +0200

    refactor: reorganize imports and enhance view structure in views.py

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -1,3 +1,15 @@
-from django.shortcuts import render
+"""app_sprava_montazi View"""
 
-# Create your views here.
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+from time import sleep
+
+
+class HomePageView(LoginRequiredMixin, TemplateView):
+    sleep(2)
+    template_name = "app_sprava_montazi/homepage.html"
+
+
+class DashboardView(LoginRequiredMixin, TemplateView):
+    sleep(2)
+    template_name = "app_sprava_montazi/dashboard.html"
