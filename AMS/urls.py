78f4d40f6f07a8038441e9d2aa9a7ce74ca1aedc
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-11 21:43:50 +0200

    feat: reorganize URL patterns and add login/logout paths for accounts

--- AMS/urls.py
+++ AMS/urls.py
@@ -2,6 +2,7 @@
 URLs config
 """
 
+from django.contrib.auth import views as auth_views
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
@@ -9,19 +10,22 @@ from django.urls import path
 from django.urls.conf import include
 
 from accounts.views import RegisterView
-from app_sprava_montazi.views import HomePageView, DashboardView
+from app_sprava_montazi.views import DashboardView, IndexView, HomePageView
 
 urlpatterns: list = [
     path("admin/", admin.site.urls),
 ]
 
 app_sprava_montazi: list = [
-    path("", HomePageView.as_view(), name="homepage"),
+    path("", IndexView.as_view(), name="index"),
+    path("homepage/", HomePageView.as_view(), name="homepage"),
     path("dashboard/", DashboardView.as_view(), name="dashboard"),
 ]
 
 app_accounts_urls: list = [
     path("accounts/register/", RegisterView.as_view(), name="signup"),
+    path("accounts/login/", auth_views.LoginView.as_view(), name="login"),
+    path("accounts/logout/", auth_views.LogoutView.as_view(), name="logout"),
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
