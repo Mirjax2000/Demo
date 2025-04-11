0ba80204eb069af03df5914fbc59527533cc0861
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-11 19:54:58 +0200

    refactor: update URLs configuration for clarity and organization

--- AMS/urls.py
+++ AMS/urls.py
@@ -1,5 +1,5 @@
 """
-URLs configuration
+URLs config
 """
 
 from django.conf import settings
@@ -9,9 +9,15 @@ from django.urls import path
 from django.urls.conf import include
 
 from accounts.views import RegisterView
+from app_sprava_montazi.views import HomePageView, DashboardView
 
 urlpatterns: list = [
     path("admin/", admin.site.urls),
+]
+
+app_sprava_montazi: list = [
+    path("", HomePageView.as_view(), name="homepage"),
+    path("dashboard/", DashboardView.as_view(), name="dashboard"),
 ]
 
 app_accounts_urls: list = [
@@ -19,7 +25,6 @@ app_accounts_urls: list = [
     path("accounts/", include("django.contrib.auth.urls")),
 ]
 
-app_sprava_montazi: list = []
 api_urls: list = []
 #
 urlpatterns += app_sprava_montazi
