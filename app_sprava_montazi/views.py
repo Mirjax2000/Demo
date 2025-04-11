f4001b708b219abb8d5829b9e18265f98f4185b4
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-11 21:43:57 +0200

    feat: remove unnecessary sleep calls from views to improve page load performance

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -2,14 +2,15 @@
 
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
-from time import sleep
+
+
+class IndexView(LoginRequiredMixin, TemplateView):
+    template_name = "base.html"
 
 
 class HomePageView(LoginRequiredMixin, TemplateView):
-    sleep(2)
     template_name = "app_sprava_montazi/homepage.html"
 
 
 class DashboardView(LoginRequiredMixin, TemplateView):
-    sleep(2)
     template_name = "app_sprava_montazi/dashboard.html"
