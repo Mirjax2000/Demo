a2ee832101f65cf2a6894e435c9f30ee189c8655
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-12 14:55:24 +0200

    feat: refactor HomePageView and DashboardView to use View class and maintain sleep functionality

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -1,16 +1,24 @@
 """app_sprava_montazi View"""
 
+from time import sleep
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.views.generic import TemplateView
+from django.shortcuts import render
+from django.views.generic import TemplateView, View
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
     template_name = "base.html"
 
 
-class HomePageView(LoginRequiredMixin, TemplateView):
-    template_name = "app_sprava_montazi/homepage.html"
+class HomePageView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        template_name = "app_sprava_montazi/homepage.html"
+        sleep(2)
+        return render(self.request, template_name)
 
 
-class DashboardView(LoginRequiredMixin, TemplateView):
-    template_name = "app_sprava_montazi/dashboard.html"
+class DashboardView(LoginRequiredMixin, View):
+    def get(self, *args, **kwargs):
+        template_name = "app_sprava_montazi/dashboard.html"
+        sleep(2)
+        return render(self.request, template_name)
