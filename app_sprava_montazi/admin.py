8913af26af8fd8f61085157ef06c1696ceb7fb60
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-16 18:49:37 +0200

    feat: enhance admin interface by adding OrderAdmin and TeamAdmin classes with search and display options

--- app_sprava_montazi/admin.py
+++ app_sprava_montazi/admin.py
@@ -1,3 +1,29 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Order, Team
+
+
+class OrderAdmin(admin.ModelAdmin):
+    search_fields = [
+        "order_number",
+        "mandant",
+        "customer_name",
+    ]
+    list_filter = ["status"]
+    list_display = ["order_number", "mandant", "customer_name", "team_type", "team"]
+
+
+class TeamAdmin(admin.ModelAdmin):
+    search_fields = ["company", "city"]
+    list_display = (
+        "company",
+        "city",
+        "region",
+        "active",
+        "price_per_hour",
+        "price_per_km",
+    )
+
+
+admin.site.register(Order, OrderAdmin)
+admin.site.register(Team, TeamAdmin)
