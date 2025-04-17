72f59c7188e2e35a861c3eb77cbbe036cd3673e7
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-17 14:04:13 +0200

    feat: update admin interface by registering DistribHubAdmin and commenting out OrderAdmin and TeamAdmin

--- app_sprava_montazi/admin.py
+++ app_sprava_montazi/admin.py
@@ -1,26 +1,30 @@
 from django.contrib import admin
 
-from .models import Order, Team
+from .models import Order, Team, DistribHub
 
 
-class OrderAdmin(admin.ModelAdmin):
-    search_fields = ["order_number"]
-    list_filter = ["status"]
-    list_display = ["order_number", "distrib_hub", "mandant", "team_type", "team"]
+# class OrderAdmin(admin.ModelAdmin):
+#     search_fields = ["order_number"]
+#     list_filter = ["status"]
+#     list_display = ["order_number", "distrib_hub", "mandant", "team_type", "team"]
 
 
-class TeamAdmin(admin.ModelAdmin):
-    search_fields = ["name", "city"]
-    list_filter = ["active"]
-    list_display = (
-        "name",
-        "city",
-        "region",
-        "active",
-        "price_per_hour",
-        "price_per_km",
-    )
+# class TeamAdmin(admin.ModelAdmin):
+#     search_fields = ["name", "city"]
+#     list_filter = ["active"]
+#     list_display = (
+#         "name",
+#         "city",
+#         "region",
+#         "active",
+#         "price_per_hour",
+#         "price_per_km",
+#     )
+class DistribHubAdmin(admin.ModelAdmin):
+    search_fields = ["code", "city"]
+    list_display = ("slug", "code", "city")
 
 
-admin.site.register(Order, OrderAdmin)
-admin.site.register(Team, TeamAdmin)
+admin.site.register(Order)
+admin.site.register(DistribHub, DistribHubAdmin)
+# admin.site.register(Team)
