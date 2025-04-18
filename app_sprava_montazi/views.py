e9541382ed190a9bf6a0e40a6ab4e4550f84c3a3
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-18 08:49:08 +0200

    feat: implement OrdersAllView with pagination for order listing

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -2,8 +2,13 @@
 
 from time import sleep
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.core.paginator import Paginator
 from django.shortcuts import render
 from django.views.generic import TemplateView, View
+from .models import Order, DistribHub
+from rich.console import Console
+
+cons: Console = Console()
 
 
 class IndexView(LoginRequiredMixin, TemplateView):
@@ -25,4 +30,11 @@ class DashboardView(LoginRequiredMixin, View):
 
 
 class OrdersAllView(LoginRequiredMixin, View):
-    pass
+    def get(self, request, *args, **kwargs):
+        orders = Order.objects.all().order_by("-created")
+        paginator = Paginator(orders, 15)
+        page_number = request.GET.get("page")
+        page_obj = paginator.get_page(page_number)
+
+        context = {"page_obj": page_obj}
+        return render(request, "app_sprava_montazi/partials/orders_all.html", context)
