4510dd156967853fd10245525dd3e131fae5ca18
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-19 19:51:17 +0200

    feat: update import_data command to use slugified column names and adjust dataset selection

--- app_sprava_montazi/management/commands/import_data.py
+++ app_sprava_montazi/management/commands/import_data.py
@@ -4,6 +4,7 @@ from pathlib import Path
 import pandas as pd
 from django.core.management.base import BaseCommand
 from rich.console import Console
+from django.utils.text import slugify
 from app_sprava_montazi.models import DistribHub, Order
 
 
@@ -18,18 +19,21 @@ class Command(BaseCommand):
         #
         dataset: pd.DataFrame = pd.read_csv(file_path, encoding="cp1250", delimiter=";")
         dataset.columns = dataset.columns.str.strip()
-        dataset = dataset[["Místo urcení", "Císlo zakázky", "Mandant"]]
-        dataset_dict: list = dataset.to_dict(orient="records")
+        dataset.columns = [slugify(col) for col in dataset.columns]
+
+        dataset = dataset[["misto-urceni", "cislo-zakazky", "mandant"]]
+        cons.print(dataset.head)
+        # dataset_dict: list = dataset.to_dict(orient="records")
         #
-        for item in dataset_dict:
-            distrib_hub_obj = DistribHub.objects.get(code=item["Místo urcení"])
-            #
-            order, created = Order.objects.get_or_create(
-                order_number=item["Císlo zakázky"],
-                distrib_hub=distrib_hub_obj,
-                mandant=item["Mandant"],
-            )
-            if created:
-                cons.log(f"{order} vytvoren")
-            else:
-                cons.log(f"{order} jiz existuje")
+        # for item in dataset_dict:
+        #     distrib_hub_obj = DistribHub.objects.get(code=item["Místo urcení"])
+        #     #
+        #     order, created = Order.objects.get_or_create(
+        #         order_number=item["Císlo zakázky"],
+        #         distrib_hub=distrib_hub_obj,
+        #         mandant=item["Mandant"],
+        #     )
+        #     if created:
+        #         cons.log(f"{order} vytvoren")
+        #     else:
+        #         cons.log(f"{order} jiz existuje")
