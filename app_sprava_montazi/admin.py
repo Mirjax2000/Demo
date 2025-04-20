1d5ef98168c5acd525cb0999ece12483260ebcaa
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-20 20:21:50 +0200

    feat: update admin panel to include Upload model and enhance Order and Team admin configurations

--- app_sprava_montazi/admin.py
+++ app_sprava_montazi/admin.py
@@ -1,33 +1,35 @@
 from django.contrib import admin
 
-from .models import Order, Team, DistribHub
+from .models import Order, Team, DistribHub, Upload
 
 
 class OrderAdmin(admin.ModelAdmin):
-    search_fields = ["order_number", "distrib_hub__slug", "mandant"]
-    # list_filter = ["status"]
-    list_display = ["order_number", "distrib_hub", "mandant"]
+    search_fields = [
+        "order_number",
+        "distrib_hub",
+        "mandant",
+        "team_type",
+        "team",
+    ]
+    list_filter = ["status"]
+    list_display = ["order_number", "distrib_hub", "mandant", "team_type", "team"]
     list_per_page = 15
 
 
-# class TeamAdmin(admin.ModelAdmin):
-#     search_fields = ["name", "city"]
-#     list_filter = ["active"]
-#       list_per_page = 15
-#     list_display = (
-#         "name",
-#         "city",
-#         "region",
-#         "active",
-#         "price_per_hour",
-#         "price_per_km",
-#     )
 class DistribHubAdmin(admin.ModelAdmin):
     search_fields = ["code", "city"]
     list_display = ("slug", "code", "city")
     list_per_page = 15
 
 
+class TeamAdmin(admin.ModelAdmin):
+    search_fields = ["name", "city"]
+    list_display = ("name", "city", "active", "price_per_hour", "price_per_km")
+    list_filter = ["active"]
+    list_per_page = 15
+
+
 admin.site.register(Order, OrderAdmin)
 admin.site.register(DistribHub, DistribHubAdmin)
-# admin.site.register(Team)
+admin.site.register(Upload)
+admin.site.register(Team, TeamAdmin)
