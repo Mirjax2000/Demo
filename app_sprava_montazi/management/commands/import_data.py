1329ad9b3bf9a8d17c3c0892bfd4e16883528262
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-21 23:00:41 +0200

    feat: enhance data import logic to include Client model and additional fields in dataset

--- app_sprava_montazi/management/commands/import_data.py
+++ app_sprava_montazi/management/commands/import_data.py
@@ -5,7 +5,7 @@ import pandas as pd
 from django.core.management.base import BaseCommand
 from rich.console import Console
 from django.utils.text import slugify
-from app_sprava_montazi.models import DistribHub, Order
+from app_sprava_montazi.models import DistribHub, Order, Client
 
 
 cons: Console = Console()
@@ -17,23 +17,48 @@ class Command(BaseCommand):
         file: str = "dataset.csv"
         file_path: Path = directory / file
         #
-        dataset: pd.DataFrame = pd.read_csv(file_path, encoding="cp1250", delimiter=";")
+        dataset: pd.DataFrame = pd.read_csv(
+            file_path,
+            encoding="cp1250",
+            delimiter=";",
+        )
         dataset.columns = dataset.columns.str.strip()
         dataset.columns = [slugify(col) for col in dataset.columns]
-
-        dataset = dataset[["misto-urceni", "cislo-zakazky", "mandant"]]
+        dataset = dataset[
+            [
+                "misto-urceni",
+                "cislo-zakazky",
+                "mandant",
+                "prijmeni",
+                "krestni-jmeno",
+                "psc",
+            ]
+        ]
+        dataset["krestni-jmeno"] = dataset["krestni-jmeno"].fillna("")
         cons.print(dataset.head)
         dataset_dict: list = dataset.to_dict(orient="records")
 
         for item in dataset_dict:
+            # ziskavam FK DistribHubu
             distrib_hub_obj = DistribHub.objects.get(code=item["misto-urceni"])
+            # vytvarim FK clienta
+            name: str = f"{item['prijmeni']} {item['krestni-jmeno']}"
+            client, client_created = Client.objects.get_or_create(
+                name=name.strip(), zip_code=item["psc"]
+            )
+            if client_created:
+                cons.log(f"{client} vytvoren", style="blue")
+            else:
+                cons.log(f"{client} jiz existuje", style="yellow")
             #
-            order, created = Order.objects.get_or_create(
+            # vytvarim Order
+            order, order_created = Order.objects.get_or_create(
                 order_number=item["cislo-zakazky"],
                 distrib_hub=distrib_hub_obj,
                 mandant=item["mandant"],
+                client=client,
             )
-            if created:
-                cons.log(f"{order} vytvoren")
+            if order_created:
+                cons.log(f"{order} vytvoren", style="blue")
             else:
-                cons.log(f"{order} jiz existuje")
+                cons.log(f"{order} jiz existuje", style="yellow")
