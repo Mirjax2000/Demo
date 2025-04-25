057e835f677bffaf3554d724bb8f3ea12078a16a
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-25 17:36:36 +0200

    refactor: remove unused navigation and search functionality from script.js

--- static/js/script.js
+++ static/js/script.js
@@ -2,19 +2,9 @@
     console.log("script.js");
     const html = document.documentElement;
     const themeToggler = document.getElementById("themeToggler");
-    const leftSide = document.getElementById("leftSide");
-    const navLinks = leftSide.querySelectorAll(".L-sidebar__nav-link");
-    const content = document.getElementById("content");
+    const orderTable = document.getElementById("orderTable");
 
 
-
-    // Navlinky - přepínání classy L-active
-    navLinks.forEach((link) => {
-        link.addEventListener("click", () => {
-            navLinks.forEach((l) => l.classList.remove("L-active"));
-            link.classList.add("L-active");
-        });
-    });
 
     function toggleTheme() {
         const current = html.dataset.theme;
@@ -30,39 +20,35 @@
 
     themeToggler.addEventListener("click", toggleTheme);
     // 
-    //  nastaveni opacity kdyz zapisujeme do searche
-    content.addEventListener("input", function (event) {
-        if (event.target && event.target.id === "inputSearch") {
-            const inputSearch = event.target;
-            const orderTable = document.getElementById("orderTable")
-            if (inputSearch.value.length >= 1) {
-                orderTable.dataset.opacity = "half"; // Nastaví hodnotu data-opacity na "half"
-            } else {
-                orderTable.dataset.opacity = "full"; // Nastaví hodnotu data-opacity na "full"
-            }
-        }
-    });
 
-    if (content) {
-        content.addEventListener('htmx:load', function () {
-            const inputSearch = content.querySelector("#inputSearch");
-            const orderTable = content.querySelector("#orderTable");
-            if (inputSearch && orderTable) {
-                if (inputSearch.value.length === 0) {
-                    orderTable.dataset.opacity = "full";
-                    console.log("HTMX obsah načten a zpracován");
-                }
-            }
-            if (orderTable) {
-                $(orderTable).DataTable({
-                    rowReorder: true,
-                    fixedColumns: true,
 
-                });
-            }
+    if (orderTable) {
+        $(orderTable).DataTable({
+
+            rowReorder: false,
+            fixedColumns: true,
+            pageLength: 15,
+            lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
+            layout: {
+                topStart: {
+                    search: {
+                        placeholder: "Hledej ..."
+                    }
+                },
+                topEnd: 'pageLength',
+                bottomStart: 'info',
+                bottomEnd: 'paging'
+            },
+            columnDefs: [
+                { orderable: false, targets: [5, 11] }
+            ],
+
+            language: {
+                emptyTable: "Žádné objednávky"
+            },
         });
-
     }
+
 })();
 
 
