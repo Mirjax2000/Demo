9a2b9bce005a56889aef10b6c24b98edabb46eb7
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-25 17:36:44 +0200

    refactor: convert HomePageView and DashboardView to use TemplateView for consistency

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -1,13 +1,13 @@
 """app_sprava_montazi View"""
 
+from typing import Any
 from rich.console import Console
+from django.db.models.query import QuerySet
 from django.contrib.auth.mixins import LoginRequiredMixin
-from django.core.paginator import Paginator
 from django.http import HttpResponse
 from django.shortcuts import render
-from django.views.generic import TemplateView, View
+from django.views.generic import ListView, TemplateView, View
 from .models import Order, DistribHub, Status
-from django.db.models import Q
 
 cons: Console = Console()
 
@@ -18,68 +18,48 @@ class IndexView(LoginRequiredMixin, TemplateView):
     template_name: str = "base.html"
 
 
-class HomePageView(LoginRequiredMixin, View):
+class HomePageView(LoginRequiredMixin, TemplateView):
     """Homepage View"""
 
-    def get(self, *args, **kwargs) -> HttpResponse:
-        template_name: str = "app_sprava_montazi/partials/homepage.html"
-        return render(self.request, template_name)
+    template_name: str = "app_sprava_montazi/homepage.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["active"] = "homepage"
+        return context
 
 
-class DashboardView(LoginRequiredMixin, View):
+class DashboardView(LoginRequiredMixin, TemplateView):
     """Dashboard View"""
 
-    def get(self, *args, **kwargs) -> HttpResponse:
-        template_name: str = "app_sprava_montazi/partials/dashboard.html"
-        return render(self.request, template_name)
+    template_name: str = "app_sprava_montazi/dashboard.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        context["active"] = "dashboard"
+        return context
 
 
-class OrdersAllView(LoginRequiredMixin, View):
+class OrdersView(LoginRequiredMixin, ListView):
     """Vypis seznamu modelu Order"""
 
-    def get(self, request, *args, **kwargs) -> HttpResponse:
+    model = Order
+    template_name = "app_sprava_montazi/orders_all.html"
+    context_object_name = "orders"
+
+    def get_queryset(self) -> QuerySet[Any]:
         orders = Order.objects.all().order_by("-created")
-        #
-        paginator: Paginator = Paginator(orders, 15)
-        page_number = request.GET.get("page", 1)
-        page_obj = paginator.get_page(page_number)
-        #
-        context: dict = {
-            "page_obj": page_obj,
-            "statuses": Status,
-        }
-        #
-        return render(request, "app_sprava_montazi/partials/orders_all.html", context)
-
-
-class OrdersSearchView(LoginRequiredMixin, View):
-    """Vypis seznamu modelu Order"""
-
-    def post(self, request, *args, **kwargs) -> HttpResponse:
-        status: str = request.POST.get("status", "").strip()
-        query: str = request.POST.get("query", "").strip().lower()
-        orders = Order.objects.all()
-        if query:
-            orders = orders.filter(
-                Q(order_number__icontains=query)
-                | Q(client__name__icontains=query)
-                | Q(distrib_hub__slug__icontains=query)
-                | Q(mandant__icontains=query)
-            )
+        status: str = self.request.GET.get("status", "").strip()
         if status:
             orders = orders.filter(status=status)
-        #
-        orders = orders.order_by("-created")
-        #
-        paginator: Paginator = Paginator(orders, 15)
-        page_number = request.GET.get("page", 1)
-        page_obj = paginator.get_page(page_number)
-        #
-        context: dict = {
-            "page_obj": page_obj,
-            "statuses": Status,
-        }
-        #
-        return render(request, "app_sprava_montazi/partials/orders_all.html", context)
+        return orders
 
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
 
+        context["statuses"] = Status
+        context["active"] = "orders_all"
+
+        return context
