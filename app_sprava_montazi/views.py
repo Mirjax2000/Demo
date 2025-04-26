f3ae276b6b64fc65d6c61446fcf61c5e2a2ddd77
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-26 20:45:10 +0200

    fix: correct active context key for TeamsView from 'team' to 'teams'

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -75,6 +75,6 @@ class TeamsView(LoginRequiredMixin, ListView):
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
-        context["active"] = "team"
+        context["active"] = "teams"
 
         return context
