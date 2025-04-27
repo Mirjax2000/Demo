85d99dbebd683880abdeefcef74631adf2d3b4bd
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-04-27 22:05:30 +0200

    feat: add success message on team creation

--- app_sprava_montazi/views.py
+++ app_sprava_montazi/views.py
@@ -3,6 +3,7 @@
 from typing import Any
 from rich.console import Console
 from django.db.models.query import QuerySet
+from django.contrib import messages
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpResponse
 from django.shortcuts import render
@@ -87,6 +88,10 @@ class TeamCreateView(LoginRequiredMixin, CreateView):
     form_class = TeamForm
     template_name = "app_sprava_montazi/partials/team_form.html"
     success_url = reverse_lazy("teams")
+
+    def form_valid(self, form):
+        messages.success(self.request, "Tým byl úspěšně vytvořen.")
+        return super().form_valid(form)
 
 
 class TeamUpdateView(LoginRequiredMixin, UpdateView):
