5f856fea80a8fc7fed22ef793fc40f10f8fce543
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-05-03 16:08:02 +0200

    fix: correct variable name and improve article toggle functionality

--- static/js/script.js
+++ static/js/script.js
@@ -7,7 +7,7 @@
     const articleTable = document.getElementById("articleTable");
     const numberInputs = document.querySelectorAll(".number");
     const messages = $(".C-messages");
-    const arcticleBtn = $('.toggle-btn');
+    const arcticleBtns = $('.toggle-btn');
 
     messages.each(function (index, element) {
         setTimeout(function () {
@@ -17,119 +17,126 @@
         }, 5000);
     });
     // articles
-    arcticleBtn.on('click', function () {
-        $(this).next('.L-form__articles__fieldset').slideToggle();
+    arcticleBtns.on('click', function () {
+        const target = $(this).next('.L-form__articles');
+
+        if (target.is(':visible')) {
+            target.slideUp();
+        } else {
+            $('.L-form__articles').slideUp();
+            target.slideDown();
+        }
     });
 
 
-// validace tel num v inputech  v HTMX
-document.body.addEventListener('htmx:afterSwap', function () {
+    // validace tel num v inputech  v HTMX
+    document.body.addEventListener('htmx:afterSwap', function () {
 
-    let numberInputs = document.querySelectorAll(".number");
-    console.log(numberInputs);
+        let numberInputs = document.querySelectorAll(".number");
+        console.log(numberInputs);
 
+        numberInputs.forEach(function (input) {
+            input.addEventListener('input', function () {
+                this.value = this.value.replace(/[^0-9]/g, '');
+            });
+        });
+    });
+
+
+    // validace tel num v inputech
     numberInputs.forEach(function (input) {
         input.addEventListener('input', function () {
             this.value = this.value.replace(/[^0-9]/g, '');
         });
     });
-});
 
 
-// validace tel num v inputech
-numberInputs.forEach(function (input) {
-    input.addEventListener('input', function () {
-        this.value = this.value.replace(/[^0-9]/g, '');
-    });
-});
+    // theme control
+    function toggleTheme() {
+        const current = html.dataset.theme;
+        const newTheme = current === "light" ? "dark" : "light";
 
+        html.dataset.theme = newTheme;
 
-// theme control
-function toggleTheme() {
-    const current = html.dataset.theme;
-    const newTheme = current === "light" ? "dark" : "light";
+        localStorage.setItem("theme", newTheme);
 
-    html.dataset.theme = newTheme;
+        html.classList.remove(current);
+        html.classList.add(newTheme);
+    }
 
-    localStorage.setItem("theme", newTheme);
+    themeToggler.addEventListener("click", toggleTheme);
+    // 
 
-    html.classList.remove(current);
-    html.classList.add(newTheme);
-}
+    if (orderTable) {
+        $(orderTable).DataTable({
+            order: [[0, 'desc']],
 
-themeToggler.addEventListener("click", toggleTheme);
-// 
-
-if (orderTable) {
-    $(orderTable).DataTable({
-        order: [[0, 'desc']],
-
-        rowReorder: false,
-        fixedColumns: true,
-        pageLength: 15,
-        lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
-        layout: {
-            topStart: {
-                search: {
-                    placeholder: "Hledej ..."
-                }
+            rowReorder: false,
+            fixedColumns: true,
+            pageLength: 15,
+            lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
+            layout: {
+                topStart: {
+                    search: {
+                        placeholder: "Hledej ..."
+                    }
+                },
+                topEnd: 'pageLength',
+                bottomStart: 'info',
+                bottomEnd: 'paging'
             },
-            topEnd: 'pageLength',
-            bottomStart: 'info',
-            bottomEnd: 'paging'
-        },
-        columnDefs: [],
-        language: {
-            emptyTable: "Žádné objednávky"
-        },
-    });
-}
-
-if (teamTable) {
-    $(teamTable).DataTable({
-        order: [[3, 'asc']],
-        rowReorder: false,
-        fixedColumns: true,
-        pageLength: 15,
-        lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
-        layout: {
-            topStart: {
-                search: {
-                    placeholder: "Hledej ..."
-                }
+            columnDefs: [],
+            language: {
+                emptyTable: "Žádné objednávky"
             },
-            topEnd: 'pageLength',
-            bottomStart: 'info',
-            bottomEnd: 'paging'
-        },
-        columnDefs: [
-            { orderable: false, targets: [6] },
-            { targets: [4, 5], type: 'num-fmt' }
-        ],
-        language: {
-            emptyTable: "Žádné Teamy",
-            decimal: ","
-        },
-    });
-}
+        });
+    }
 
-if (articleTable) {
-    $(articleTable).DataTable({
-        rowReorder: false,
-        fixedColumns: true,
-        searching: false,
-        paging: false,
+    if (teamTable) {
+        $(teamTable).DataTable({
+            order: [[3, 'asc']],
+            rowReorder: false,
+            fixedColumns: true,
+            pageLength: 15,
+            lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
+            layout: {
+                topStart: {
+                    search: {
+                        placeholder: "Hledej ..."
+                    }
+                },
+                topEnd: 'pageLength',
+                bottomStart: 'info',
+                bottomEnd: 'paging'
+            },
+            columnDefs: [
+                { orderable: false, targets: [6] },
+                { targets: [4, 5], type: 'num-fmt' }
+            ],
+            language: {
+                emptyTable: "Žádné Teamy",
+                decimal: ","
+            },
+        });
+    }
 
-        columnDefs: [
-            { orderable: false, targets: [3] },
-        ],
-        language: {
-            emptyTable: "Žádné Artikly !!!",
-        },
-    });
-}
+    if (articleTable) {
+        $(articleTable).DataTable({
+            rowReorder: false,
+            fixedColumns: true,
+            searching: false,
+            paging: false,
 
-}) ();
+            columnDefs: [
+                { orderable: false, targets: [3] },
+            ],
+            language: {
+                emptyTable: "Žádné Artikly !!!",
+            },
+        });
+    }
+
+})();
 
 
 
