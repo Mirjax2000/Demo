5f2fc7b367c9ec191c011c07f8b47d44c0bf2de5
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-05-26 14:43:10 +0200

    sconto template

--- app_sprava_montazi/protokols_OOP.py
+++ app_sprava_montazi/protokols_OOP.py
@@ -2,27 +2,30 @@
 
 from io import BytesIO
 from pathlib import Path
-from django.conf import settings
 from dataclasses import dataclass
 from rich.console import Console
+from django.conf import settings
 from reportlab.pdfgen.canvas import Canvas
+from reportlab.lib.colors import HexColor
 from reportlab.lib.pagesizes import A4
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.ttfonts import TTFont
 
+# ---
 cons: Console = Console()
 
 
+# ---
 @dataclass
 class PdfConfig:
     font_size_small: float = 7.7
     font_size_normal: float = 8
-    font_size_bigger: float = 10
-    font_size_biggest: float = 12
+    font_size_bigger: float = 11.5
     x_offset: float = 40
     y_offset: float = 40
     width: float = A4[0]
     height: float = A4[1]
+    files: Path = settings.BASE_DIR / "files"
 
 
 class PdfGenerator:
@@ -40,7 +43,7 @@ class PdfGenerator:
 
     def generate_pdf_sconto(self) -> bytes:
         self.section.header()  # --- header ---
-        # self.section.sconto()  # --- body sconto ---
+        self.section.sconto()  # --- body sconto ---
         self.section.footer()  # --- footer ---
         # ---
         self.cvs.showPage()
@@ -88,23 +91,43 @@ class Section:
         return y_offset + self.bottom_offset
 
     def draw_text(
-        self, text: str, y_offset: float, font="Roboto-Regular", font_size=None
+        self,
+        text: str,
+        y_offset: float,
+        x_offset=None,
+        font="Roboto-Regular",
+        font_size=None,
     ) -> None:
         """Vykresluje text"""
         if font_size is None:
             font_size = self.cfg.font_size_normal
+        if x_offset is None:
+            x_offset = self.cfg.x_offset
         self.cvs.setFont(font, font_size)
-        self.cvs.drawString(self.cfg.x_offset, self.from_top_offset(y_offset), text)
+        self.cvs.drawString(x_offset, self.from_top_offset(y_offset), text)
 
     def header(self) -> None:
         """header section"""
-        self.draw_text("RHENUS Home Delivery s.r.o.", 0, "Roboto-Semibold")
-        self.draw_text("Plzeňská 256, 252 19 Chrášťany", 15, "Roboto-Regular")
-        self.draw_text("IČO: 27629325", 11)
-        self.draw_text("DIČ: CZ27629325", 11)
+        self.draw_text(
+            "RHENUS Home Delivery s.r.o.", y_offset=11, font="Roboto-Semibold"
+        )
+        self.draw_text(
+            "Plzeňská 256, 252 19 Chrášťany", y_offset=15, font="Roboto-Regular"
+        )
+        self.draw_text("IČO: 27629325", y_offset=11)
+        self.draw_text("DIČ: CZ27629325", y_offset=11)
         self.draw_text(
             "Společnost je zapsaná v OR pod spisovou značkou C 120048 vedená u Městského soudu v Praze",
-            11,
+            y_offset=11,
+        )
+        image_width: float = 180
+        image_height: float = 42
+        self.cvs.drawImage(
+            image=self.cfg.files / "rhenus_logo.png",
+            x=self.cfg.width - 180 - self.cfg.x_offset,
+            y=self.cfg.height - self.cfg.y_offset - image_height,
+            width=image_width,
+            height=image_height,
         )
 
     def footer(self) -> None:
@@ -119,7 +142,6 @@ class Section:
         self.cvs.drawString(right_margin, self.cfg.y_offset, "podpis zákazníka")
         # --- lines ---
 
-        cons.log(self.bottom_offset)
         self.cvs.setLineWidth(1)
         self.cvs.setDash(1, 2)
         self.cvs.line(
@@ -145,39 +167,36 @@ class Section:
 
     def sconto(self) -> None:
         """Sconto section"""
-        self.cvs.setFont("Roboto-Semibold", self.cfg.font_size_bigger)
-        self.cvs.drawString(
-            self.cfg.x_offset,
-            self.cfg.height - self.cfg.x_offset,
+        self.draw_text(
             "Předávací protokol provedených prací",
+            y_offset=22,
+            font="Roboto-Semibold",
+            font_size=self.cfg.font_size_bigger,
         )
-        # ---
-        self.cvs.setFont("Roboto-Semibold", self.cfg.font_size_normal)
-        self.cvs.drawString(
-            53,
-            self.cfg.height - self.cfg.x_offset,
+        self.draw_text(
             "Kontaktní údaje zákazníka",
+            y_offset=22,
+            font="Roboto-Semibold",
+            font_size=self.cfg.font_size_normal,
         )
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
+        self.draw_text("Jméno:", y_offset=15)
+        self.draw_text("Ulice a čp/čo:", y_offset=15)
+        self.draw_text("PSČ:", y_offset=15)
+        self.draw_text("Město:", y_offset=15)
+        self.draw_text("Telefon:", y_offset=15)
+        self.draw_text("E-mail:", y_offset=15)
         # ---
-        self.cvs.setFont("Roboto-Regular", self.cfg.font_size_normal)
-        self.cvs.drawString(53, self.cfg.height - self.cfg.x_offset, "Jméno:")
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
+        self.cvs.setStrokeColor(HexColor("#ABABAB"))
+        self.cvs.roundRect(
+            37, self.from_top_offset(5), 200, 105, radius=4, stroke=1, fill=0
+        )
         # ---
-        self.cvs.drawString(53, self.cfg.height - self.cfg.x_offset, "Ulice a čp/čo:")
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
-        # ---
-        self.cvs.drawString(53, self.cfg.height - self.cfg.x_offset, "PSČ:")
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
-        # ---
-        self.cvs.drawString(53, self.cfg.height - self.cfg.x_offset, "Město:")
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
-        # ---
-        self.cvs.drawString(53, self.cfg.height - self.cfg.x_offset, "Telefon:")
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
-        # ---
-        self.cvs.drawString(53, self.cfg.height - self.cfg.x_offset, "E-mail:")
-        self.cvs.rect(50, self.cfg.height - self.cfg.x_offset - 3, 200, 20)
+        text_1 = "Zákazník prohlašuje, že má-li být zboží připevněno či zavěšeno na stěně, že se ujistil před zahájením prací o povaze a průběhu rozvodů předmětných médií, "
+        text_2 = "o nosnosti stěn a případných místních specifikách. Zákazník je povinen informovat Společnost o všech těchto skutečnostech významných pro montáž"
+        text_3 = "před zahájením montážních prací."
+        self.draw_text(text_1, y_offset=15, font_size=self.cfg.font_size_small)
+        self.draw_text(text_2, y_offset=11, font_size=self.cfg.font_size_small)
+        self.draw_text(text_3, y_offset=11, font_size=self.cfg.font_size_small)
 
     def general(self) -> None:
         """general pdf section"""
