287ea370846d55ce5ef4c1786481b9ac4d4ff751
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-06-09 21:18:36 +0200

    update

--- AMS/settings.py
+++ AMS/settings.py
@@ -169,7 +169,8 @@ MESSAGE_TAGS = {
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "miroslav.viktorin77@gmail.com"
-EMAIL_HOST_PASSWORD = "uvvz alzi uexu ggeb"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
 EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "Automate with Django <miroslav.viktorin77@gmail.com>"
