b2cd09f329696687ae1937e8511aafb2a9fe9b11
Miroslav Viktorin <miroslav.viktori@seznam.cz>
2025-07-02 19:09:03 +0200

    makame

--- accounts/views.py
+++ accounts/views.py
@@ -2,12 +2,14 @@
 
 import logging
 from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse_lazy
 from django.views.generic.edit import FormView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.contrib.auth.views import LoginView
 from django.contrib import messages
+
+# --- formulare
+from .forms import CustomUserCreationForm
 
 # ---
 logger_login = logging.getLogger("login")
@@ -41,11 +43,10 @@ class CustomLoginView(LoginView):
 
 class RegisterView(LoginRequiredMixin, FormView):
     template_name = "registration/register.html"
-    form_class = UserCreationForm
+    form_class = CustomUserCreationForm
     success_url = reverse_lazy("index")
 
     def form_valid(self, form):
         user = form.save()
         messages.success(self.request, f"Uzivatel {user} vytvořen.")
-        login(self.request, user)
         return super().form_valid(form)
