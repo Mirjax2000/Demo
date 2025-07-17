a60c3ab6c40d0099e03a02b1b528108955ac657c
vigo <vigo@example.com>
2025-07-17 13:13:04 +0200

    upravy zpet

--- AMS/settings.py
+++ AMS/settings.py
@@ -162,9 +162,6 @@ STATIC_URL = "/static/"
 STATICFILES_DIRS = [BASE_DIR / "static"]
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
-
-STATICFILES_STORAGE = "django.contrib.staticfiles.storage.ManifestStaticFilesStorage"
-
 MEDIA_URL = "/media/"
 MEDIA_ROOT = BASE_DIR / "media/"
 
