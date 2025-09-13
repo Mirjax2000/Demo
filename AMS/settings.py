9983e896bfb128f18a7ea391ba5da91b203d5bc0
vigo <vigo@example.com>
2025-09-13 21:59:23 +0200

    time expiration a 24 hodin

--- AMS/settings.py
+++ AMS/settings.py
@@ -60,14 +60,15 @@ INSTALLED_APPS = [
 REST_FRAMEWORK = {
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
     "DEFAULT_AUTHENTICATION_CLASSES": [
-        "API.auth.ExpiringTokenAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
     ],
     "DEFAULT_PERMISSION_CLASSES": [
         "rest_framework.permissions.IsAuthenticated",
     ],
 }
 
-TOKEN_EXPIRE_MINUTES = 1
+TOKEN_EXPIRE_MINUTES = 1440
+
 
 PHONENUMBER_DEFAULT_REGION = "CZ"
 PHONENUMBER_DB_FORMAT = "E164"
