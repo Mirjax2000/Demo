cc2d62554e84460a318018858e566c93309eff07
vigo <vigo@example.com>
2025-09-20 11:00:02 +0200

    js uklid

---
+++ static/js/modules.js
@@ -0,0 +1,150 @@
+(function () {
+
+    console.log("modules.js loaded");
+
+    const orderTable = document.getElementById("orderTable");
+    const teamTable = document.getElementById("teamTable");
+    const clientOrderTable = document.getElementById("clientOrderTable");
+
+    // DataTable for orders
+    if (orderTable) {
+        $(orderTable).DataTable({
+            // zapiname server side proccesing
+            processing: true,
+            serverSide: true,
+            // ajax mi posle do requestu json s pozadavky na strankovani
+            // odpoved bude qs s aplikovanyma filtrama 
+            ajax: {
+                url: window.location.href,
+                type: "GET",
+            },
+
+            order: [[3, 'desc']],
+            rowReorder: false,
+            fixedColumns: false,
+            pageLength: 15,
+            lengthMenu: [[15, 20, 30], [15, 20, 30]],
+            layout: {
+                topStart: {
+                    search: {
+                        placeholder: "Hledej ..."
+                    }
+                },
+                topEnd: 'pageLength',
+                bottomStart: 'info',
+                bottomEnd: 'paging'
+            },
+            columns: [
+                { data: 'order_number', orderable: false, searchable: true },
+                { data: 'distrib_hub', orderable: true, searchable: true },
+                { data: 'mandant', orderable: true, searchable: true },
+                { data: 'evidence_termin', orderable: true, searchable: true },
+                { data: 'delivery_termin', orderable: true, searchable: true },
+                { data: 'client', orderable: true, searchable: true },
+                { data: 'team_type', orderable: true, searchable: false },
+                { data: 'team', orderable: true, searchable: true },
+                { data: 'montage_termin', orderable: true, searchable: true },
+                { data: 'status', orderable: true, searchable: false },
+                { data: 'articles', orderable: false, searchable: false },
+                { data: 'notes', orderable: false, searchable: false },
+            ],
+            language: {
+                emptyTable: "Žádné objednávky",
+                info: "<small>od <strong>_START_</strong> do <strong>_END_</strong> z <strong>_TOTAL_</strong> záznamů (filtr z <strong>_MAX_</strong> záznamů)</small>",
+                infoFiltered: "",
+                infoEmpty: "",
+                lengthMenu: "<small>_MENU_ zakázek na stránku</small>",
+                search: "Vyhledávání: "
+            },
+        });
+        // Delegace - nasloucháme klikům na odkazy v tabulce
+        orderTable.addEventListener("click", function (e) {
+            const link = e.target.closest(".copy_link_order_number");
+
+            if (link) {
+                const text = link.innerText.trim();
+
+                navigator.clipboard.writeText(text).then(() => {
+                }).catch(() => {
+                    console.log("❌ Kopírování selhalo.");
+                });
+            }
+        });
+    }
+
+    // datatables for client orders
+    if (clientOrderTable) {
+        $(clientOrderTable).DataTable({
+            order: [[3, 'desc']],
+            rowReorder: false,
+            fixedColumns: false,
+            pageLength: 5,
+            lengthMenu: [[5, 10, 15, -1], [5, 10, 15, "Vše"]],
+            layout: {
+                topStart: {
+                    search: {
+                        placeholder: "Hledej ..."
+                    }
+                },
+                topEnd: 'pageLength',
+                bottomStart: 'info',
+                bottomEnd: 'paging'
+            },
+            columnDefs: [
+                { targets: [3, 4, 7], type: 'date' },
+                { targets: [0, 9, 10], orderable: false }],
+            language: {
+                emptyTable: "Žádné objednávky",
+                decimal: ",",
+                info: "Zobrazuji _START_ až _END_ z _TOTAL_ záznamů (filtr z _MAX_ záznamů)",
+                infoFiltered: "",
+                infoEmpty: "",
+                search: "Vyhledávání: ",
+                lengthMenu: "_MENU_ zakázek na stránku",
+            },
+        });
+    }
+
+    // DataTable for teams
+    if (teamTable) {
+        $(teamTable).DataTable({
+            order: [[3, 'asc']],
+            rowReorder: false,
+            fixedColumns: false,
+            pageLength: 15,
+            lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
+            layout: {
+                topStart: {
+                    search: {
+                        placeholder: "Hledej ..."
+                    }
+                },
+                topEnd: 'pageLength',
+                bottomStart: 'info',
+                bottomEnd: 'paging'
+            },
+            columnDefs: [
+                { orderable: false, targets: [6] },
+                { targets: [4, 5], type: 'num-fmt' }
+            ],
+            language: {
+                emptyTable: "Žádné Teamy",
+                decimal: ",",
+                info: "Zobrazuji _START_ až _END_ z _TOTAL_ záznamů (filtr z _MAX_ záznamů)",
+                infoFiltered: "",
+                infoEmpty: "",
+                lengthMenu: "_MENU_ zakázek na stránku",
+                search: "Vyhledávání: "
+            },
+        });
+    }
+
+    // galerie lightbox
+    lightbox.option({
+        'resizeDuration': 200,
+        'imageFadeDuration': 200,
+        'wrapAround': true,
+        "fitImagesInViewport": true,
+    })
+
+})();
--- static/js/script.js
+++ static/js/script.js
@@ -1,10 +1,7 @@
 (function () {
-    console.log("script.js");
+    console.log("script.js loaded");
     const html = document.documentElement;
     const themeToggler = document.getElementById("themeToggler");
-    const orderTable = document.getElementById("orderTable");
-    const clientOrderTable = document.getElementById("clientOrderTable");
-    const teamTable = document.getElementById("teamTable");
     const numberInputs = document.querySelectorAll(".number");
     const deleteBtns = document.querySelectorAll(".form__delete");
     const P_main = document.querySelector(".P-main");
@@ -273,140 +270,8 @@
         html.classList.add(newTheme);
     }
 
-    if (themeToggler) { // Ensure themeToggler exists
+    if (themeToggler) {
         themeToggler.addEventListener("click", toggleTheme);
-    }
-    // datatables for client orders
-    if (clientOrderTable) {
-        $(clientOrderTable).DataTable({
-            order: [[3, 'desc']],
-            rowReorder: false,
-            fixedColumns: false,
-            pageLength: 5,
-            lengthMenu: [[5, 10, 15, -1], [5, 10, 15, "Vše"]],
-            layout: {
-                topStart: {
-                    search: {
-                        placeholder: "Hledej ..."
-                    }
-                },
-                topEnd: 'pageLength',
-                bottomStart: 'info',
-                bottomEnd: 'paging'
-            },
-            columnDefs: [
-                { targets: [3, 4, 7], type: 'date' },
-                { targets: [0, 9, 10], orderable: false }],
-            language: {
-                emptyTable: "Žádné objednávky",
-                decimal: ",",
-                info: "Zobrazuji _START_ až _END_ z _TOTAL_ záznamů (filtr z _MAX_ záznamů)",
-                infoFiltered: "",
-                infoEmpty: "",
-                search: "Vyhledávání: ",
-                lengthMenu: "_MENU_ zakázek na stránku",
-            },
-        });
-    }
-    // 
-    // DataTable for orders
-    if (orderTable) {
-        $(orderTable).DataTable({
-            // zapiname server side proccesing
-            processing: true,
-            serverSide: true,
-            // ajax mi posle do requestu json s pozadavky na strankovani
-            // odpoved bude qs s aplikovanyma filtrama 
-            ajax: {
-                url: window.location.href,
-                type: "GET",
-            },
-
-            order: [[3, 'desc']],
-            rowReorder: false,
-            fixedColumns: false,
-            pageLength: 15,
-            lengthMenu: [[15, 20, 30], [15, 20, 30]],
-            layout: {
-                topStart: {
-                    search: {
-                        placeholder: "Hledej ..."
-                    }
-                },
-                topEnd: 'pageLength',
-                bottomStart: 'info',
-                bottomEnd: 'paging'
-            },
-            columns: [
-                { data: 'order_number', orderable: false, searchable: true },
-                { data: 'distrib_hub', orderable: true, searchable: true },
-                { data: 'mandant', orderable: true, searchable: true },
-                { data: 'evidence_termin', orderable: true, searchable: true },
-                { data: 'delivery_termin', orderable: true, searchable: true },
-                { data: 'client', orderable: true, searchable: true },
-                { data: 'team_type', orderable: true, searchable: false },
-                { data: 'team', orderable: true, searchable: true },
-                { data: 'montage_termin', orderable: true, searchable: true },
-                { data: 'status', orderable: true, searchable: false },
-                { data: 'articles', orderable: false, searchable: false },
-                { data: 'notes', orderable: false, searchable: false },
-            ],
-            language: {
-                emptyTable: "Žádné objednávky",
-                info: "<small>od <strong>_START_</strong> do <strong>_END_</strong> z <strong>_TOTAL_</strong> záznamů (filtr z <strong>_MAX_</strong> záznamů)</small>",
-                infoFiltered: "",
-                infoEmpty: "",
-                lengthMenu: "<small>_MENU_ zakázek na stránku</small>",
-                search: "Vyhledávání: "
-            },
-        });
-        // Delegace - nasloucháme klikům na odkazy v tabulce
-        orderTable.addEventListener("click", function (e) {
-            const link = e.target.closest(".copy_link_order_number");
-
-            if (link) {
-                const text = link.innerText.trim();
-
-                navigator.clipboard.writeText(text).then(() => {
-                }).catch(() => {
-                    console.log("❌ Kopírování selhalo.");
-                });
-            }
-        });
-    }
-
-    // DataTable for teams
-    if (teamTable) {
-        $(teamTable).DataTable({
-            order: [[3, 'asc']],
-            rowReorder: false,
-            fixedColumns: false,
-            pageLength: 15,
-            lengthMenu: [[15, 20, 30, -1], [15, 20, 30, "Vše"]],
-            layout: {
-                topStart: {
-                    search: {
-                        placeholder: "Hledej ..."
-                    }
-                },
-                topEnd: 'pageLength',
-                bottomStart: 'info',
-                bottomEnd: 'paging'
-            },
-            columnDefs: [
-                { orderable: false, targets: [6] },
-                { targets: [4, 5], type: 'num-fmt' }
-            ],
-            language: {
-                emptyTable: "Žádné Teamy",
-                decimal: ",",
-                info: "Zobrazuji _START_ až _END_ z _TOTAL_ záznamů (filtr z _MAX_ záznamů)",
-                infoFiltered: "",
-                infoEmpty: "",
-                lengthMenu: "_MENU_ zakázek na stránku",
-                search: "Vyhledávání: "
-            },
-        });
     }
 
     // Article formset
@@ -437,7 +302,6 @@
         const left = document.querySelector('.left');
         const right = document.querySelector('.right');
         if (left && right) {
-            console.log(left, right)
             right.style.maxHeight = left.offsetHeight + 50 + 'px';
         }
     }
@@ -698,7 +562,6 @@
         });
     });
 
-
     //  --- protokol img
     $(document).ready(function () {
         const protocolImg = $('#protocolImg');
@@ -829,6 +692,7 @@
             }
         })
     }
+
     // zaterminovano s dopravou na Realizovano function
     function advicedRealizedOrder() {
         const checkBoxRealizovanoSDopravou = document.getElementById("checkBoxRealizovanoSDopravou");
@@ -1007,14 +871,5 @@
             detailProfit.classList.add("zero");
         }
     }
-    // galerie lightbox
-    lightbox.option({
-        'resizeDuration': 200,
-        'imageFadeDuration': 200,
-        'wrapAround': true,
-        "fitImagesInViewport": true,
-    })
-
-
 
 })();
